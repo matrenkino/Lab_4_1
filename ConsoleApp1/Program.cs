113ad7c85b56aef61fe31449a255bf241cd3be56
matrenkino <yamatrenkino@yandex.ru>
2023-12-14 13:02:57 +0700

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,43 +1,76 @@
 ﻿using System;
+using LAB1;
+using LAB2;
+using LAB3;
+using LAB4;
 
-class Program
+namespace Calculator
 {
-    static void Main()
+    class Program
     {
-        char choice;
-        double a = 0;
-        double b = 0;
-
-        do
+        static void Main()
         {
-            Console.WriteLine("Меню выбора:");
-            Console.WriteLine("6. Разделить A на B");
-            Console.WriteLine("7. Выйти из программы");
+            char choice;
+            double a = 0;
+            double b = 0;
 
-            Console.Write("Введите номер операции: ");
-            choice = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-
-            switch (choice)
+            do
             {
-                case '6':
-                    if (b != 0)
-                    {
-                        Console.WriteLine($"Частное A и B = {a / b}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("На ноль делить нельзя!");
-                    }
-                    break;
-                case '7':
-                    Console.WriteLine("Программа завершена.");
-                    break;
-                default:
-                    Console.WriteLine("Неправильный выбор. Пожалуйста, выберите существующий пункт меню.");
-                    break;
-            }
-            Console.WriteLine();
-        } while (choice != '7');
+                Console.WriteLine("Меню выбора:");
+                Console.WriteLine("1. Ввести A");
+                Console.WriteLine("2. Ввести B");
+                Console.WriteLine("3. Сложить A и B");
+                Console.WriteLine("4. Вычесть B из A");
+                Console.WriteLine("5. Умножить A на B");
+                Console.WriteLine("6. Разделить A на B");
+                Console.WriteLine("7. Выйти из программы");
+
+                Console.Write("Введите номер операции: ");
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case '1':
+                        Console.Write("Введите значение A: ");
+                        a = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"A = {a}");
+                        break;
+                    case '2':
+                        Console.Write("Введите значение B: ");
+                        b = double.Parse(Console.ReadLine());
+                        Console.WriteLine($"B = {b}");
+                        break;
+                    case '3':
+                        Console.WriteLine($"Сумма A и B = {a + b}");
+                        break;
+                    case '4':
+                        Console.WriteLine($"Разность A и B = {a - b}");
+                        break;
+                    case '5':
+                        Console.WriteLine($"Произведение A и B = {a * b}");
+                        break;
+                    case '6':
+                        if (b != 0)
+                        {
+                            Console.WriteLine($"Частное A и B = {a / b}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("На ноль делить нельзя!");
+                        }
+                        break;
+                    case '7':
+                        Console.WriteLine("Программа завершена.");
+                        break;
+                    default:
+                        Console.WriteLine("Неправильный выбор. Пожалуйста, выберите существующий пункт меню.");
+                        break;
+                }
+                Console.WriteLine();
+            } while (choice != '7');
+        }
     }
 }
+
+
