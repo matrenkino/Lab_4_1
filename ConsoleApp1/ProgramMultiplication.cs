53f580118736d05d2030b507eebe2f269e2fbc73
matrenkino <yamatrenkino@yandex.ru>
2023-12-14 12:26:47 +0700

    UpdateMul

--- ConsoleApp1/ProgramMultiplication.cs
+++ ConsoleApp1/ProgramMultiplication.cs
@@ -1,36 +1,13 @@
 ﻿using System;
 
-class Program
+namespace LAB
 {
-    static void Main()
+    class Program
     {
-        char choice;
-        double a = 0;
-        double b = 0;
 
-        do
+        public double Multiplication(double a, double b)
         {
-            Console.WriteLine("Меню выбора:");
-            Console.WriteLine("5. Умножить A на B");
-            Console.WriteLine("7. Выйти из программы");
-
-            Console.Write("Введите номер операции: ");
-            choice = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-
-            switch (choice)
-            {
-                case '5':
-                    Console.WriteLine($"Произведение A и B = {a * b}");
-                    break;
-                case '7':
-                    Console.WriteLine("Программа завершена.");
-                    break;
-                default:
-                    Console.WriteLine("Неправильный выбор. Пожалуйста, выберите существующий пункт меню.");
-                    break;
-            }
-            Console.WriteLine();
-        } while (choice != '7');
+            return a * b;
+        }
     }
 }
