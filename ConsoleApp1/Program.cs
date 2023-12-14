397ae1ed610342c535510b26a2c492cdb464927d
matrenkino <yamatrenkino@yandex.ru>
2023-12-14 11:19:35 +0700

    add choice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,41 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Введите ваше имя:");
-        string userName = Console.ReadLine();
+        char choice;
+        double a = 0;
+        double b = 0;
 
-        Console.WriteLine($"Привет, {userName}!");
+        do
+        {
+            Console.WriteLine("Меню выбора:");
+            Console.WriteLine("1. Ввести A");
+            Console.WriteLine("2. Ввести B");
+            Console.WriteLine("7. Выйти из программы");
+
+            Console.Write("Введите номер операции: ");
+            choice = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case '1':
+                    Console.Write("Введите значение A: ");
+                    a = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"A = {a}");
+                    break;
+                case '2':
+                    Console.Write("Введите значение B: ");
+                    b = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"B = {b}");
+                    break;
+                case '7':
+                    Console.WriteLine("Программа завершена.");
+                    break;
+                default:
+                    Console.WriteLine("Неправильный выбор. Пожалуйста, выберите существующий пункт меню.");
+                    break;
+            }
+            Console.WriteLine();
+        } while (choice != '7');
     }
 }
