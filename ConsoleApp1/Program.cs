76b5e2fed6723a09fd5d9a689c9320c4c9bbc410
matrenkino <yamatrenkino@yandex.ru>
2023-12-14 10:31:15 +0700

    first project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите ваше имя:");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine($"Привет, {userName}!");
+    }
+}
