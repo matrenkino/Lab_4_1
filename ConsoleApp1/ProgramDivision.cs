ff4aead43df4aed4a1654db68d3fddf085e4f4ee
matrenkino <yamatrenkino@yandex.ru>
2023-12-14 12:27:41 +0700

    UpdateDiv

--- ConsoleApp1/ProgramDivision.cs
+++ ConsoleApp1/ProgramDivision.cs
@@ -1,43 +1,13 @@
 ﻿using System;
 
-class Program
+namespace LAB
 {
-    static void Main()
+    class Program
     {
-        char choice;
-        double a = 0;
-        double b = 0;
 
-        do
+        public double Division(double a, double b)
         {
-            Console.WriteLine("Меню выбора:");
-            Console.WriteLine("6. Разделить A на B");
-            Console.WriteLine("7. Выйти из программы");
-
-            Console.Write("Введите номер операции: ");
-            choice = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-
-            switch (choice)
-            {
-                case '6':
-                    if (b != 0)
-                    {
-                        Console.WriteLine($"Частное A и B = {a / b}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("На ноль делить нельзя!");
-                    }
-                    break;
-                case '7':
-                    Console.WriteLine("Программа завершена.");
-                    break;
-                default:
-                    Console.WriteLine("Неправильный выбор. Пожалуйста, выберите существующий пункт меню.");
-                    break;
-            }
-            Console.WriteLine();
-        } while (choice != '7');
+            return a / b;
+        }
     }
 }
