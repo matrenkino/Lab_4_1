1582fcb18c8e974c4e6e03b49f95fee8f48b3611
matrenkino <yamatrenkino@yandex.ru>
2023-12-14 12:22:35 +0700

    UpdateAdd

--- ConsoleApp1/ProgramAddition.cs
+++ ConsoleApp1/ProgramAddition.cs
@@ -1,36 +1,13 @@
 ﻿using System;
-
+namespace LAB {
 class Program
 {
     static void Main()
     {
-        char choice;
-        double a = 0;
-        double b = 0;
-
-        do
-        {
-            Console.WriteLine("Меню выбора:");
-            Console.WriteLine("3. Сложить A и B");
-            Console.WriteLine("7. Выйти из программы");
-
-            Console.Write("Введите номер операции: ");
-            choice = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-
-            switch (choice)
-            {
-                case '3':
-                    Console.WriteLine($"Сумма A и B = {a + b}");
-                    break;
-                case '7':
-                    Console.WriteLine("Программа завершена.");
-                    break;
-                default:
-                    Console.WriteLine("Неправильный выбор. Пожалуйста, выберите существующий пункт меню.");
-                    break;
-            }
-            Console.WriteLine();
-        } while (choice != '7');
+        public double Addition (double a, double b)
+	{
+		return a + b;
+	}
     }
+}
 }
